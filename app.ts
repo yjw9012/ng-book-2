35abea1ef84c59714031d24b72c64124c126ce0d
Jiwoong Youn <yjw9012job@gmail.com>
2016-07-14 00:07:44 -0700

    FIrst time running the app

--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
-// your code goes here
+import {bootstrap} from "@angular/platform-browser-dynamic";
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "hello-world",
+    template: `
+        <div>
+            Hello World!
+        </div>
+    `
+})
+
+class HelloWorld {
+
+}
+
+bootstrap(HelloWorld);
