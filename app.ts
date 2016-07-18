3e81a6aa4e2cc055f9db96167890744c5e3ba853
Jiwoong Youn <yjw9012job@gmail.com>
2016-07-17 17:52:28 -0700

    Create RedditApp component

--- app.ts
+++ app.ts
@@ -2,22 +2,34 @@ import {bootstrap} from "@angular/platform-browser-dynamic";
 import {Component} from "@angular/core";
 
 @Component({
-    selector: "hello-world",
+    selector: "reddit",
     template: `
-        <ul>
-            <li *ngFor="let name of names">
-                Hello {{name}}
-            </li> 
-        </ul>
+        <form class="ui large form segment">
+            <h3 class="ui header">Add a Link</h3>
+            <div class="field">
+                <label for="title">Title:</label>
+                <input name="title" #newTitle>
+            </div>
+            <div class="field">
+                <label for="link">Link:</label>
+                <input name="link" #newLink>
+            </div>
+            <button (click)="addArticle(newTitle, newLink)"
+                    class="ui positive right floated button">
+                Submit
+            </button>
+        </form>
     `
 })
 
-class HelloWorld {
-    names : string[];
+class RedditApp {
 
     constructor() {
-        this.names = ["Jiwoong", "Dustin", "Youn", "Light"];
+    }
+
+    addArticle(newTitle: HTMLInputElement, newLink: HTMLInputElement) : void {
+        console.log(`Adding article: newTitle="${newTitle.value}" / newLink="${newLink.value}"`);
     }
 }
 
-bootstrap(HelloWorld);
+bootstrap(RedditApp);
