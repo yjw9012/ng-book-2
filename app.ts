4872e3bb0aa1d034cd192ca979d0aff9c821be7e
Jiwoong Youn <yjw9012job@gmail.com>
2016-07-17 18:21:18 -0700

    Create Reddit Article component

--- app.ts
+++ app.ts
@@ -2,7 +2,68 @@ import {bootstrap} from "@angular/platform-browser-dynamic";
 import {Component} from "@angular/core";
 
 @Component({
+    selector: "reddit-article",
+    host: {
+        class: "row"
+    },
+    template: `
+        <div class="four wide column center aligned votes">
+            <div class="ui statistic">
+                <div class="value">
+                    {{votes}}
+                </div>
+                <div class="label">
+                    Points
+                </div>
+            </div>
+        </div>
+        <div class="twelve wide column">
+            <a class="ui large header" href="{{link}}">
+                {{title}}
+            </a>
+            <ul class="ui big horizontal list voters">
+                <li class="item">
+                    <a href (click)="voteUp()">
+                        <i class="arrow up icon"></i>
+                        upvote
+                    </a>
+                </li>
+                <li class="item">
+                    <a href (click)="voteDown()">
+                        <i class="arrow down icon"></i>
+                        downvote
+                    </a>
+                </li>
+            </ul>
+        </div>
+    `
+})
+
+class ArticleComponent {
+    votes: number;
+    title: string;
+    link: string;
+
+    constructor() : void {
+        this.title = "Angular 2";
+        this.link = "http://angular.io";
+        this.votes = 10;
+    }
+
+    voteUp() : boolean {
+        this.votes += 1;
+        return false;
+    }
+
+    voteDown() : boolean {
+        this.votes -= 1;
+        return false;
+    }
+}
+
+@Component({
     selector: "reddit",
+    directives: [ArticleComponent],
     template: `
         <form class="ui large form segment">
             <h3 class="ui header">Add a Link</h3>
@@ -19,6 +80,10 @@ import {Component} from "@angular/core";
                 Submit
             </button>
         </form>
+
+        <div class="ui grid posts">
+            <reddit-article></reddit-article>
+        </div>
     `
 })
 
